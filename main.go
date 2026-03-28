d400808c188c25f4dbc0962a44e3225cccf9052b
Tumurug Aurel <111057774+Aur71@users.noreply.github.com>
2026-03-28 10:38:30 +0200

    Removed fiber

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"github.com/gofiber/fiber/v2"
+	"fmt"
 )
 
 func main() {
-	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	log.Fatal(app.Listen(":3000"))
+	fmt.Println("Hello World!")
 }
